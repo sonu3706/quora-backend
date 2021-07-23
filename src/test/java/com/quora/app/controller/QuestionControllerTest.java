1b543ab9d6ade8ab77952447a4c966ec5d480db6
Chandan Mishra <chandanmishra3706@gmail.com>
2021-07-23 17:04:36 +0530

    Test cases added

--- src/test/java/com/quora/app/controller/QuestionControllerTest.java
+++ src/test/java/com/quora/app/controller/QuestionControllerTest.java
@@ -1,4 +1,52 @@
 package com.quora.app.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.quora.app.models.Question;
+import com.quora.app.services.QuestionService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@AutoConfigureMockMvc
 public class QuestionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private QuestionService mockQuestionService;
+
+    // Parsing String format data into JSON format
+    private static String jsonToString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testPostQuestionSuccess() throws Exception {
+        when(mockQuestionService.postQuestion(any(Question.class))).thenReturn(Boolean.TRUE);
+        MvcResult mvcResult = mockMvc.perform(post("/api/v1/questions/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonToString(Question.class)))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
 }
--- src/test/java/com/quora/app/service/AuthServiceImplTest.java
+++ src/test/java/com/quora/app/service/AuthServiceImplTest.java
@@ -1,4 +1,75 @@
 package com.quora.app.service;
 
+import com.quora.app.exceptions.AuthException;
+import com.quora.app.models.UserAuth;
+import com.quora.app.repositories.AuthRepository;
+import com.quora.app.services.AuthService;
+import com.quora.app.services.TokenService;
+import com.quora.app.services.impls.AuthServiceImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class AuthServiceImplTest {
+
+    public AuthService authService;
+    @Mock
+    private AuthRepository authRepository;
+    @Mock
+    private TokenService tokenService;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        authService = new AuthServiceImpl(authRepository, tokenService);
+    }
+
+    @After
+    public void tearDown() {
+        authRepository = null;
+        tokenService = null;
+        authService = null;
+    }
+
+    @Test
+    public void testCreateUserAccountSuccess() {
+        when(authRepository.findUserAuthsByUserEmail("chandan@abc.com")).thenReturn(Optional.empty());
+        Boolean status = authService.createUserAccount(createUserAuthObject());
+        Assert.assertNotNull(status);
+        Assert.assertTrue(status);
+
+        verify(authRepository, times(1)).findUserAuthsByUserEmail("chandan@abc.com");
+        verify(authRepository, times(1)).save(any());
+        verifyNoMoreInteractions(authRepository);
+    }
+
+    @Test(expected = AuthException.UserAlreadyExists.class)
+    public void testCreateUserAccountFailure() {
+        when(authRepository.findUserAuthsByUserEmail("chandan@abc.com")).thenReturn(Optional.ofNullable(createUserAuthObject()));
+        authService.createUserAccount(createUserAuthObject());
+    }
+
+    @Test
+    public void testLoginUserSuccess() {
+
+    }
+
+    @Test
+    public void testLoginUserFailure() {
+
+    }
+
+    private UserAuth createUserAuthObject() {
+        return UserAuth.builder().userEmail("chandan@abc.com").userPassword("12345678").build();
+    }
 }
